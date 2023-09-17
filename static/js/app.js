3b474af6665c23f328307b75034882cc1606eb1b
aarononward <129822330+aarononward@users.noreply.github.com>
2023-09-17 16:41:51 -0700

    adding finalized javascript file

--- static/js/app.js
+++ static/js/app.js
@@ -1,125 +1,213 @@
 //Fetch JSON data
-let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-d3.json(url).then(function(data) {
-    
-    //use .filter and multiple functions for developing plots and creating ids and labels
-    //
-    let metadata = data.metadata
-    function(choice) {
-        result = metadata.filter(sample => sample.id == choice)
-        result.otu_ids        
-    }
-    
+let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-    for (let i=0; i<metadata.length;i++){
-        let sample_metadata = metadata[i]
-        // console.log(sample_metadata)
-    }
-    for (let i=0; i<samples.length; i++){
-        let sample = samples[i]
-        let sample_values = sample.sample_values
-        let otu_ids = sample.otu_ids
-        let otu_labels = sample.otu_labels
-        let sample_id = sample.id
+//Create the table creator function
+function chartbuilder(cow){
+    
+    //Pull the json file
+    d3.json(url).then(function(data) {
+    
+    //Save the samples section of the json data   
+    let samples = data.samples;
+    
+    //Match the chosen id to the sample id
+    let sample = samples.filter(cat => cat.id == cow)[0];
+    
+    //Save the sample_values list of each sample to a variable
+    let sample_values = sample.sample_values;
+    
+    //Same the otu_ids list of each sample to a variable
+    let otu_ids = sample.otu_ids
+
+    //Save the otu_labels list of sample to a variable
+    let otu_labels = sample.otu_labels;
+    
+    //Create the bar chart
+    let bar = [{
+
+        //Use otu_ids as the x values for the bar chart.
+        x: sample_values.slice(0,10).reverse(),   
+        
+        //Use sample_values as the y values for the bar chart.
+        y: otu_ids.slice(0,10).map(otu_ids => `OTU ${otu_ids}`).reverse(),
+        
+        //Use otu_labels as the hovertext for the chart.
+        text : otu_labels,  
+        
+        //set the type of chart to bar chart
+        orientation : 'h',
+
+        type : 'bar'
+        
+
+        }]
     
     
-    
-    function init(){
-        let samples = data.samples
-        let bar = [{
-            //Use sample_values as the values for the bar chart.
-             x: samples[0].sample_values,
-            //Use otu_ids as the labels for the bar chart.
-             y: samples[0].otu_ids,
-            //Use otu_labels as the hovertext for the chart.
-            text : [samples[0].otu_labels],
+    //Add the plot to the page
+    Plotly.newPlot("bar",bar);  
+      
+    //Create the bubble chart
+    let bubble = [{
+
+        //Use otu_ids for the x values.
+        x : otu_ids.slice(0,10),
+
+        //Use sample_values for the y values.
+        y: sample_values.slice(0,10),
+        
+        //Set the x labels
+        labels : "OTU ID",
+
+        //Set the mode as markers
+        mode: 'markers',
+
+        //Set the marker parameters
+        marker: {
             
-            type: "bar"
-        }]
-        Plotly.newPLot("bar",bar)  
+            //Use sample_values for the marker size.
+            size : sample_values.slice(0,10),
+            
+            //Use otu_ids for the marker colors.
+            color : otu_ids.slice(0,10)
+        },
+    
+        //Use otu_labels for the text values.
+        text: otu_labels.slice(0,10),
+
+    }];
+
+    //Create layout parameters
+    let layout = {
         
-        let bubble = [{
-             //Use otu_ids for the x values.
-             x : samples[0].otu_ids,
-             //Use sample_values for the y values.
-             y: samples[0].sample_values,
-             mode: 'markers',
-             marker: {
-                 //Use sample_values for the marker size.
-                 size : samples[0].sample_values,
-                 //Use otu_ids for the marker colors.
-                 color : samples[0].otu_ids,
-             },
-             //Use otu_labels for the text values.
-             text: [samples[0].otu_labels] 
-        }]
-        Plotly.newPLot("bubble",bubble)
+        //Set the title for the x axis
+        xaxis:
+            {title:
+                {text: "OTU IDs"}}
     }
+    
+    //Add the plot to the page
+    Plotly.newPlot("bubble",bubble,layout);
+    
+    });
+};
 
-    d3.selectAll("#selDataset").on("change",optionChanged);
+//Create a function to clear the Dempgraphic table
+function clearDemoTable(){
+    
+    //Connect to the table container 
+    let panel_body = document.getElementById('sample-metadata');
+    
+    //Clear the table container
+    panel_body.innerHTML = '';
 
-    function optionChanged(){
-        let dropdownMenu = d3.select("#selDataset")
-        let dataset = dropdownMenu.property("value")
+};
+
+//Create a function to fill the Demographic table
+function demobuilder(goat){    
+    
+    //Grab the json data
+    d3.json(url).then(function(data) {
         
-        if(dataset === sample_id){
-            let row = document.createElement('tr');
-            let id
-            let bar = sample
-            let bubble = sample}
+        //Save the Metadata array to a variable
+        let metadata = data.metadata;
         
-
+        //Match the chosen id to the sample id
+        let metasample = metadata.filter(sheep => sheep.id == goat)[0];
         
+        //Put a table section into the HTML
+        let table = document.createElement('table');
+        
+        //Create the tbody element to fill with the demo data
+        let tbody = table.createTBody();
+       
+        //Create a function to populate the table with the demo data
+        for (let key in metasample){ 
 
-  
+            //Check for the keys in the metasample dictionary
+            if (metasample.hasOwnProperty(key)) {
                 
-            //Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-            let trace1 = {
-                //Use sample_values as the values for the bar chart.
-                x: sample_values,
-                //Use otu_ids as the labels for the bar chart.
-                y: otu_ids,
-                //Use otu_labels as the hovertext for the chart.
-                text : [otu_labels],
-                type: "bar",
-            }    
+                //Grab each value in the dictionary
+                let value = metasample[key];
 
-            // let bar = [trace1]
-            Plotly.restyle("bar","x",[x])
-            Plotly.restyle("bar","y",[y])
+                //Create a row for the table
+                let row = tbody.insertRow();
 
-            //Create a bubble chart that displays each sample.
-            let trace2  = {
-                //Use otu_ids for the x values.
-                x : otu_ids,
-                //Use sample_values for the y values.
-                y: sample_values,
-                mode: 'markers',
-                marker: {
-                    //Use sample_values for the marker size.
-                    size : sample_values,
-                    //Use otu_ids for the marker colors.
-                    color : otu_ids,
-                },
-                //Use otu_labels for the text values.
-                text: [otu_labels] 
+                //Connect to the first table cell
+                let cell1 = row.insertCell(0);
+                
+                //Connect to the second table cell
+                let cell2 = row.insertCell(1);
+                
+                //Connect to the third table cell
+                let cell3 = row.insertCell(2)
+                
+                //Fill the first cell with a key
+                cell1.textContent = key;
+                
+                //Fill the second cell with a semicolon
+                cell2.textContent = " : "
+                
+                //Fill the third cell with the corresponding value
+                cell3.textContent = value
             }
-            // let bubble = [trace2]
+        //Connect to the table container  
+        let panel_body = document.getElementById('sample-metadata');
+        
+        //Add the table to the container
+        panel_body.appendChild(table);
+        };
+           
+        
+    });
+        
+        
+};
 
-            Plotly.restyle("bubble", "x",[x])
-            Plotly.restyle("bubble","y",[y])
+//Create a function to call the other functions if the chosen id is changed
+function optionChanged(pig){
+    
+    //Clear the demo table
+    clearDemoTable(pig);
 
-            }       
-        }
-    }
-})
+    //Fill charts
+    chartbuilder(pig);
+    
+    //Fill the demographics table
+    demobuilder(pig);
+    
+};
+
+//Fill the page once it's open
+function init(){
+    
+    //Connect to the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    //Grab the json data   
+    d3.json(url).then(function(data) {
+
+            //Grabs the is from the Names array
+            let names = data.names;
+            
+            //Loop through the names array
+            for (let i=0; i<names.length;i++){
+                
+                //Add an option to the table
+                dropdownMenu.append('option')
+                
+                //Add the id as an option
+                .text(names[i]).property("value",names[i]);
+            }
+            
+            //Build the charts
+            chartbuilder(names[0]);
+            
+            //Fill the Demographic table
+            demobuilder(names[0]);
+        }      
+    )};
+
 
 init()
-//Display the sample metadata, i.e., an individual's demographic information.
 
-//Display each key-value pair from the metadata JSON object somewhere on the page.
-
-//Update all the plots when a new sample is selected. 
-
-//Additionally, you are welcome to create any layout that you would like for your dashboard
 
